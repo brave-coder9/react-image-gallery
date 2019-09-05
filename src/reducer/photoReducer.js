cdde02e2355803e7daee8080246a55ec3ca96a94
besmir22 <besmir@dataskeptic.com>
2019-09-05 17:49:26 +0800

    initial reducer

---
+++ src/reducer/photoReducer.js
@@ -0,0 +1,19 @@
+import * as ACTION_TYPE from '../action';
+
+const initialState = {
+  photos: [],
+  error: undefined,
+}
+
+export const photoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPE.FETCH_PHOTO_SUCCESS: {
+      return { ...state, photos: action.payload };
+    }
+    case ACTION_TYPE.FETCH_PHOTO_ERROR: {
+      return { ...state, photos: [], error: action.payload };
+    }
+    default:
+      return state;
+  }
+};
