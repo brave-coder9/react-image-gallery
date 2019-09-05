b63a378e87b3b50ff80eb2921c8f8e7d37014a9c
besmir22 <besmir@dataskeptic.com>
2019-09-05 18:53:07 +0800

    unit-test for photoReducer

--- src/reducer/photoReducer.js
+++ src/reducer/photoReducer.js
@@ -8,7 +8,7 @@ const initialState = {
 export const photoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_TYPE.FETCH_PHOTO_SUCCESS: {
-      return { ...state, photos: action.payload };
+      return { ...state, photos: action.payload, error: undefined };
     }
     case ACTION_TYPE.FETCH_PHOTO_ERROR: {
       return { ...state, photos: [], error: action.payload };
